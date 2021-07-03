1dd593e856545dd139e9ed94f76649f8887764a7
sledesma <sebas@Scorpion.sebas.com>
2021-07-03 11:49:27 -0300

    Casi estamos

---
+++ Clase3/php/functions.php
@@ -0,0 +1,7 @@
+<?php
+
+function home_obtener_posts($posts) {
+  // Las llaves de una funcion PHP son, prácticamente, OTRO CÓDIGO
+
+  return $posts;
+}
--- Clase3/php/index.php
+++ Clase3/php/index.php
@@ -1,4 +1,7 @@
 <?php
 /**
- * Licencia
+ * data/ => Se encarga ÚNICAMENTE del origen de datos 
+ * functions/ => Se encarga ÚNICAMENTE de procesar esos datos (Lógica de negocio)
+ * ../about.php, ../contact.php, ../index.php, ../post.php => Se encarga ÚNICAMENTE de mostrar los datos
  */
+
