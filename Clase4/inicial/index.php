0d0310234f738d1433c00422db7628ad0065d442
sledesma <sebas@Scorpion.sebas.com>
2021-07-10 11:35:16 -0300

    index

--- Clase4/inicial/index.php
+++ Clase4/inicial/index.php
@@ -1,200 +1,63 @@
-<!DOCTYPE html>
-<html>
-	<head>
-	<title>ComercioIT | Tu E-Shop en PHP</title>
-	<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all" />
-	<!--theme-style-->
-	<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />	
-	<!--//theme-style-->
-	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
-	<!--fonts-->
-	<link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800' rel='stylesheet' type='text/css'>
-	<!--//fonts-->
-	<script src="js/jquery.min.js"></script>
-	<!--script-->
-	</head>
-	<body> 
-		<!--header-->
-		<div class="header">
-			<div class="bottom-header">
-				<div class="container">
-					<div class="header-bottom-left">
-						<div class="logo"><a href="index.php">Comercio<strong>IT</strong></a></div>
-						<!--div class="search">
-							<input type="text" name="q">
-							<input type="submit" value="BUSCAR">
-						</div-->
-						<div class="clearfix"></div>
-					</div>
-					<div class="header-bottom-right">					
-						<!--div class="account">
-							<a href="ingreso.html"><span></span> TU CUENTA</a>
-						</div-->
-						<ul class="login">
-							<li><a href="ingreso.php"><span></span> INGRESAR</a></li>
-							&nbsp;|&nbsp;
-							<li><a href="registro.php">REGISTRARME</a></li>
-							&nbsp;|&nbsp;
-							<li><a href="contacto.php">CONTACTO</a></li>
-						</ul>
-						<!--div class="cart"><a href="#"><span></span>CART</a></div-->
-						<div class="clearfix"></div>
-					</div>
-					<div class="clearfix"></div>	
-				</div>
-			</div>
-		</div>
-		<!---->
-		<div class="container">
-			<section id="page">
-				<!-- PRODUCTOS DESTACADOS -->
-<div class="shoes-grid">
-	<div class="products">
-		<h5 class="latest-product">PRODUCTOS DESTACADOS</h5>
-	</div>
-	<div class="product-left">
-		<!-- Producto #1 -->
-		<div class="col-sm-4 col-md-4 chain-grid">
-			<a href="producto.php"><img class="img-responsive chain" src="images/productos/P001.jpg" alt=" " /></a>
-			<div class="grid-chain-bottom">
-				<h6><a href="producto.php">Lorem ipsum dolor #1</a></h6>
-				<div class="star-price">
-					<div class="dolor-grid"> 
-						<span class="actual">300$</span>
-					</div>
-					<a class="now-get get-cart" href="#">VER MÁS</a> 
-					<div class="clearfix"></div>
-				</div>
-			</div>
-		</div>
-		<!-- Producto #2 -->
-		<div class="col-sm-4 col-md-4 chain-grid">
-			<a href="producto.php"><img class="img-responsive chain" src="images/productos/P002.jpg" alt=" " /></a>
-			<div class="grid-chain-bottom">
-				<h6><a href="producto.php">Lorem ipsum dolor #2</a></h6>
-				<div class="star-price">
-					<div class="dolor-grid"> 
-						<span class="actual">300$</span>
-					</div>
-					<a class="now-get get-cart" href="#">VER MÁS</a> 
-					<div class="clearfix"></div>
-				</div>
-			</div>
-		</div>
-		<!-- Producto #3 -->
-		<div class="col-sm-4 col-md-4 chain-grid grid-top-chain">
-			<a href="producto.php"><img class="img-responsive chain" src="images/productos/P003.jpg" alt=" " /></a>
-			<div class="grid-chain-bottom">
-				<h6><a href="producto.php">Lorem ipsum dolor #3</a></h6>
-				<div class="star-price">
-					<div class="dolor-grid"> 
-						<span class="actual">300$</span>
-					</div>
-					<a class="now-get get-cart" href="#">VER MÁS</a> 
-					<div class="clearfix"></div>
-				</div>
-			</div>
-		</div>
-		<div class="clearfix"></div>
-	</div>
-	<div class="clearfix"> </div>
-</div>
-<!-- ULTIMOS PRODUCTOS -->
-<div class="shoes-grid">
-	<div class="products">
-		<h5 class="latest-product">ULTIMOS PRODUCTOS</h5>	
-		<a class="view-all" href="productos.php">VER TODOS<span></span></a>
-	</div>
-	<div class="product-left">
-		<!-- Producto #1 -->
-		<div class="col-sm-4 col-md-4 chain-grid">
-			<a href="producto.php"><img class="img-responsive chain" src="images/productos/P004.jpg" alt=" " /></a>
-			<span class="star"></span>
-			<div class="grid-chain-bottom">
-				<h6><a href="producto.php">Lorem ipsum dolor #1</a></h6>
-				<div class="star-price">
-					<div class="dolor-grid"> 
-						<span class="actual">300$</span>
-					</div>
-					<a class="now-get get-cart" href="#">VER MÁS</a> 
-					<div class="clearfix"></div>
-				</div>
-			</div>
-		</div>
-		<!-- Producto #2 -->
-		<div class="col-sm-4 col-md-4 chain-grid">
-			<a href="producto.php"><img class="img-responsive chain" src="images/productos/P005.jpg" alt=" " /></a>
-			<span class="star"></span>
-			<div class="grid-chain-bottom">
-				<h6><a href="producto.php">Lorem ipsum dolor #2</a></h6>
-				<div class="star-price">
-					<div class="dolor-grid"> 
-						<span class="actual">300$</span>
-					</div>
-					<a class="now-get get-cart" href="#">VER MÁS</a> 
-					<div class="clearfix"></div>
-				</div>
-			</div>
-		</div>
-		<!-- Producto #3 -->
-		<div class="col-sm-4 col-md-4 chain-grid grid-top-chain">
-			<a href="producto.php"><img class="img-responsive chain" src="images/productos/P006.jpg" alt=" " /></a>
-			<span class="star"></span>
-			<div class="grid-chain-bottom">
-				<h6><a href="producto.php">Lorem ipsum dolor #3</a></h6>
-				<div class="star-price">
-					<div class="dolor-grid"> 
-						<span class="actual">300$</span>
-					</div>
-					<a class="now-get get-cart" href="#">VER MÁS</a> 
-					<div class="clearfix"></div>
-				</div>
-			</div>
-		</div>
-		<div class="clearfix"></div>
-	</div>
-	<div class="clearfix"> </div>
-</div>
-			</section>
-		</div>
+<?php
+/**
+ * El principal aporte de este archivo
+ * es ofrecer un UNICO punto de acceso.
+ * Todas las peticiones HTTP serán dirigidas a este archivo
+**/
 
-		<!---->
-		<div class="footer">
-			<div class="footer-top">
-				<div class="container">
-					<div class="latter">
-						<h6>LA MEJOR MANERA DE COMPRAR ONLINE!</h6>
-						<div class="clearfix"></div>
-					</div>
-					<div class="latter-right">
-						<p>SEGUINOS</p>
-						<ul class="face-in-to">
-							<li><a href="http://www.twitter.com/educacionit"><span></span></a></li>
-							<li><a href="http://www.facebook.com/educacionIT"><span class="facebook-in"></span></a></li>
-							<div class="clearfix"></div>
-						</ul>
-						<div class="clearfix"></div>
-					</div>
-					<div class="clearfix"></div>
-				</div>
-			</div>
-			<div class="footer-bottom">
-				<div class="container">
-					<div class="footer-bottom-cate cate-bottom">
-						<h6>DIRECCIÓN</h6>
-						<ul>
-							<li>Lavalle 648, 8° Piso</li>
-							<li>C.A.B.A.</li>
-							<li>Buenos Aires</li>
-							<li>Argentina</li>
-							<li class="phone">(011) 4328-0457</li>
-						</ul>
-					</div>
-					<div class="clearfix"></div>
-				</div>
-			</div>
-		</div>
-		<!--initiate accordion-->
-		<script src="js/custom.js"></script>
-	</body>
-</html>
+// Determinar la petición actual
+$accion = $_GET['a'];
+
+// Enviar la respuesta correspondiente a dicha petición
+
+require_once $accion.".php";
+
+/*
+
+PHP cuenta con 4 funciones para incluir código
+incluir código = añadir código al código que se está ejecutando ANTES de que éste se ejecute
+Imprescindible = PHP Debe DETENERSE si no encuentra el archivo
+
+1.require_once
+¿Es imprescindible?         =>  SI
+¿Se necesita una sola vez?  =>  SI
+
+2.require
+¿Es imprescindible?         =>  SI
+¿Se necesita una sola vez?  =>  NO
+
+3.include_once
+¿Es imprescindible?         =>  NO
+¿Se necesita una sola vez?  =>  SI
+
+4.include
+¿Es imprescindible?         =>  NO
+¿Se necesita una sola vez?  =>  NO
+
+
+*/
+
+
+/*
+switch($accion) {
+  case 'home':
+    include "home.php";
+    break;
+  case 'ingreso':
+    include "ingreso.php";
+    break;
+  case 'producto':
+    include "producto.php";
+    break;
+  case 'productos':
+    include "productos.php";
+    break;
+  case 'registro':
+    include "registro.php";
+    break;
+  default:
+    include "home.php";
+    break;
+}
+*/
+
