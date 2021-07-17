27955dc92a4dc54c1d32f0406b8bd388873444c9
sledesma <sebas@Scorpion.sebas.com>
2021-07-17 12:32:11 -0300

    csv_read_all

---
+++ Clase5/csv/csv.php
@@ -0,0 +1,20 @@
+<?php
+
+function csv_read_all($ruta) {
+  $stream = fopen($ruta, 'r');
+  $cabeceras = false;
+  $salida = [];
+  $actual = fgetcsv($stream); // lee el inicial
+  while ($actual != false){ 
+    if($cabeceras == false) {
+      // Si cabeceras no tiene un valor, la fila actual serán las cabeceras
+      $cabeceras = $actual;
+    } else {
+      $resultado = array_combine($cabeceras, $actual);
+      array_push($salida, $resultado);
+    }
+    $actual = fgetcsv($stream); // lee el siguiente
+  }; 
+  fclose($stream);
+  return $salida;
+}
