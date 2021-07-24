e9f0cd61b8a1ac06183b28b5d6695e6e293215f7
sledesma <sebas@Scorpion.sebas.com>
2021-07-24 11:39:55 -0300

    Correcciones

--- Clase 6/tienda_juegos.sql
+++ Clase 6/tienda_juegos.sql
@@ -3,8 +3,12 @@ create database tienda_juegos;
 use tienda_juegos;
 
 create table juego (
-codArticulo TEXT,
-genero TEXT,
-nombre TEXT,
-stock INT,
-fechaAlta TIMESTAMP DEFAULT current_timestamp()
+  codArticulo TEXT,
+  genero TEXT,
+  nombre TEXT,
+  stock INT,
+  fechaAlta TIMESTAMP DEFAULT current_timestamp()
+)
+
+insert into juego (codArticulo, genero, nombre, stock)
+values ('A-150', 'Estrategia', 'Age of Empires II', 20);
