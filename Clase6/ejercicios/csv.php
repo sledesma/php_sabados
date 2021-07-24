29fbe7ba73b616386420f85a54bf1fa12133ecc4
sledesma <sebas@Scorpion.sebas.com>
2021-07-24 13:50:51 -0300

    Final clase 6

---
+++ Clase6/ejercicios/csv.php
@@ -0,0 +1,25 @@
+<?php
+// CLASE 5
+/**
+ * $ruta (string) - La ruta del archivo .csv
+ * 
+ * Retorna: un array asociativo en donde las claves se obtienen a partir de la primera fila
+ */
+function csv_read_all($ruta) {
+  $stream = fopen($ruta, 'r');
+  $cabeceras = false;
+  $salida = [];
+  $actual = fgetcsv($stream); // lee el inicial
+  while ($actual != false){ 
+    if($cabeceras == false) {
+      // Si cabeceras no tiene un valor, la fila actual serán las cabeceras
+      $cabeceras = $actual;
+    } else {
+      $resultado = array_combine($cabeceras, $actual);
+      array_push($salida, $resultado);
+    }
+    $actual = fgetcsv($stream); // lee el siguiente
+  }; 
+  fclose($stream);
+  return $salida;
+}
---
+++ Clase6/ejercicios/index.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once 'csv.php'; // Clase 5
+
+if(isset($_GET['sym'])) { // Clase 1/2
+
+  $ruta = 'http://clasico.rava.com/empresas/precioshistoricos.php?e='.urlencode($_GET['sym']).'&csv=1';
+
+  $datos = csv_read_all($ruta); // Clase 5
+
+  $windowSize = 10;
+
+  foreach ($datos as $i => $hist) { // Clase 2 / 3 => Recorre todos los registros
+    if($i >= $windowSize) {
+      $currentWindow = [];
+
+      echo "<b>Fecha actual: ".$hist['fecha']." </b>";
+
+      echo " | ";
+
+      echo "<i>Cierre: ".$hist['cierre']."</i>";
+
+      echo " | ";
+
+      for ($j=$windowSize; $j >= 1 ; $j--) { 
+          array_push($currentWindow, $datos[$i - $j]['cierre']);
+      }
+      
+      $media = array_sum($currentWindow) / count($currentWindow);
+      echo "<i>Media: ".$media." </i>";
+
+      echo "<hr />";
+    } else {
+      echo "NO puedo operar<br>";
+    }
+  }
+
+} else {
+  echo 'Por favor, introducir el símbolo como parámetro de la URL sym';
+}
