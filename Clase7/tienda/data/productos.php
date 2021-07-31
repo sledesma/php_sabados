69ae2a7b22903d7065460aad17063de49d96d70a
sledesma <sebas@Scorpion.sebas.com>
2021-07-31 11:08:57 -0300

    data/productos

---
+++ Clase7/tienda/data/productos.php
@@ -0,0 +1,11 @@
+<?php
+
+function data_productos_getall() {
+  return [
+    [ 'id' => 1, 'descripcion' => 'Zapatos', 'precio' => 2500 ],
+    [ 'id' => 2, 'descripcion' => 'Remeras', 'precio' => 2000 ],
+    [ 'id' => 3, 'descripcion' => 'Lentes', 'precio' => 4500 ],
+    [ 'id' => 4, 'descripcion' => 'Mouse', 'precio' => 500 ],
+    [ 'id' => 5, 'descripcion' => 'Teclado', 'precio' => 1000 ],
+  ];
+}
